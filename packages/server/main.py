90a8278dd56bd5f0d0274367c77c6ec4e2c7ce22
Al <31523262+anmilleriii@users.noreply.github.com>
2022-02-27 15:39:27 -0800

    Feature/add fastapi server (#4)
    * feat(api): add FastAPI and init pipenv
    
    * feat(api): add Forest ORM model and init alembic
    
    * feat(api): add Forest schema
    
    * feat(api): add API server service
    
    * feat(api): add /forest and /forest/uuid routes in progress, add initial migration
    
    * feat(db): add (very silly) mock data to see postgres
    
    * fix(db): fix primary key duplicate w/mock data error

---
+++ packages/server/main.py
@@ -0,0 +1,72 @@
+# todo the api
+#
+import os
+import typing
+from uuid import UUID
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi_sqlalchemy import DBSessionMiddleware
+from fastapi_sqlalchemy import db
+
+from models import Forest as ModelForest
+from schema import Forest as SchemaForest
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+app = FastAPI()
+
+app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
+
+import json
+
+
+def apply_mocks():
+    """
+    For development / instead of distributing a database dump, seeding data from mocks.json each run.
+    """
+    with db():
+
+        with open("./data/mocks.json") as mocks_file:
+            mocks_data = json.load(mocks_file)
+            for forest in mocks_data["forests"]:
+                db_forest = ModelForest(**forest)
+                db.session.add(db_forest)
+                db.session.commit()
+    return
+
+# only apply mocks if not already seeded.
+with db():
+    if not db.session.query(ModelForest).count() > 0:
+        apply_mocks()
+    
+
+
+@app.get("/forest")
+def get_all_forests(offset: int = 0, limit: int = 2):
+    """
+    TODO: test pagination
+    TODO: in production, would not overfetch (e.g., could use GraphQL or better query parameters)
+    """
+    forests = db.session.query(ModelForest).offset(offset).limit(limit).all()
+    return forests
+
+
+@app.get("/forest/{forest_uuid}", response_model=SchemaForest)
+def get_forest(forest_uuid: UUID):
+    """
+    TODO test
+    """
+    forest = db.session.query(ModelForest).filter(ModelForest.uuid == forest_uuid).first()
+
+    if not forest:
+        raise HTTPException(404, "Forest with UUID: {forest_uuid} not found.")
+
+    return forest
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port="8000")
---
+++ packages/server/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Text
+import uuid
+
+Base = declarative_base()
+
+
+class Forest(Base):
+    __tablename__ = "forests"
+    uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    country = Column(String)
+    type = Column(String)
+    short_description = Column(String)
+    # location todo, geo or string?
+    image_url = Column(String, nullable=True)
+    covered_area = Column(Integer, nullable=True)
+    carbon_stored = Column(Integer, nullable=True)
+    change_in_30_days = Column(Integer, nullable=True)
+    long_description = Column(Text, nullable=True)
