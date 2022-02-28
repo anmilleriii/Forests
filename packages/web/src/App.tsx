d6943879c8ed5bfdb7c822c88fc9651774c69f36
Al Miller <albert.miller@uconn.edu>
2022-02-28 11:40:21 -0800

    feat(general): add vite @ alias, scaffold react router

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,11 +1,19 @@
-import { ChakraProvider , Box, Select} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Directory from "@/views/Directory";
+import Details from "@/views/Details";
+import NotFound from "@/views/NotFound";
 
 export default function App() {
   return (
     <ChakraProvider>
-      <Box>ff</Box>
-      <Select>asdf</Select>
-      <div className="red">asdf</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Directory />} />
+          <Route path="/:country" element={<Details />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
