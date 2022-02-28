05a2c6bbeb6b570b5c936cb48b27a33c6bf8dd47
Al Miller <albert.miller@uconn.edu>
2022-02-28 11:24:13 -0800

    feat(chakra): add chakra-ui remove tachyons (faster ui development for this)

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,7 +1,11 @@
+import { ChakraProvider , Box, Select} from "@chakra-ui/react";
+
 export default function App() {
   return (
-    <>
+    <ChakraProvider>
+      <Box>ff</Box>
+      <Select>asdf</Select>
       <div className="red">asdf</div>
-    </>
+    </ChakraProvider>
   );
 }
